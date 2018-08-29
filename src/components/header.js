06904cd1a5b2811e1f1839520b847725a4081023
Brandon Ferrer <brandonferrer@peachjar.com>
2018-08-29 00:40:26 -0700

    Header changes

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,52 @@
 import React from 'react'
+import { css } from 'react-emotion'
 import Link from 'gatsby-link'
 
-const Header = ({ siteTitle }) => (
+const Header = () => (
   <div
     style={{
-      background: 'rebeccapurple',
+      backgroundColor: 'black',
       marginBottom: '1.45rem',
     }}
   >
     <div
       style={{
         margin: '0 auto',
-        maxWidth: 960,
         padding: '1.45rem 1.0875rem',
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+      <Link to="/" className={styles.link}>
+        <h1 className={styles.logo}>
+          BRANDON FERRER
+          <sup className={styles.sup}>
+            <span className={styles.tm}>TM</span>
+          </sup>
+        </h1>
+      </Link>
     </div>
   </div>
 )
+
+const styles = {
+  logo: css`
+    font-family: Nunito Sans !important;
+    font-size: 1rem;
+    font-weight: 700;
+    letter-spacing: 1.2px;
+    color: white;
+    margin: 0;
+  `,
+  link: css`
+    text-decoration: none;
+  `,
+  tm: css`
+    font-size: 0.5rem;
+    font-weight: bold;
+  `,
+  sup: css`
+    top: -0.5rem;
+    left: 0.25rem;
+  `,
+}
 
 export default Header
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { css } from 'react-emotion'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
@@ -14,22 +15,36 @@ const Layout = ({ children, data }) => (
         { name: 'keywords', content: 'sample, something' },
       ]}
     />
-    <Header siteTitle={data.site.siteMetadata.title} />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
+    <Header />
+    <div className={styles.wrapper}>
+      <h1 className={styles.header}>"BLOG"</h1>
+      <div className={styles.postWrapper}>{children()}</div>
     </div>
   </div>
 )
 
 Layout.propTypes = {
   children: PropTypes.func,
+}
+
+const styles = {
+  header: css`
+    font-family: Nunito Sans !important;
+    font-weight: 700;
+    letter-spacing: 1.2px;
+    color: black;
+  `,
+  wrapper: css`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-direction: column;
+    padding: 3rem;
+  `,
+  postWrapper: css`
+    justify-content: flex-start;
+    align-self: flex-start;
+  `,
 }
 
 export default Layout
