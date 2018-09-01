2c8b99c08049fb104b35effbd7972d4b31294fc5
Brandon Ferrer <brandonferrer@peachjar.com>
2018-09-01 14:53:47 -0700

    PhotoCard revision

--- src/components/instagram/PhotoCard.jsx
+++ src/components/instagram/PhotoCard.jsx
@@ -1,29 +1,40 @@
 import React from 'react'
 import _ from 'lodash'
 import { Card, Icon, Image } from 'semantic-ui-react'
-// TODO:
 
 const PhotoCard = ({
   caption,
   likes,
   date,
   imageUrlLow,
-  imageUrlStandard,
-  imageUrlThumb,
+  // imageUrlStandard,
+  // imageUrlThumb,
   igLink,
 }) => (
   <Card>
-    <Image src={imageUrlLow} />
+    <Image
+      src={imageUrlLow}
+      style={{
+        objectFit: 'cover',
+        maxHeight: '320px',
+        width: '320px',
+      }}
+    />
     <Card.Content>
+      <Card.Meta>
+        <span>{date}</span>
+      </Card.Meta>
       <Card.Description>
-        {_.truncate(caption, {
-          length: 24,
-          separator: ' ',
-        })}
+        <p style={{ fontSize: '12px' }}>
+          {_.truncate(caption, {
+            length: 38,
+            separator: ' ',
+          })}
+        </p>
       </Card.Description>
     </Card.Content>
     <Card.Content extra>
-      <a>
+      <a href={igLink} target="_blank" rel="noreferrer noopener">
         <Icon name="heart" />
         {likes}
       </a>
