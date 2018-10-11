299eff4417eefb378ebd9b4bddc660dfd8e5c028
Brandon Ferrer <brandonferrer@peachjar.com>
2018-10-11 00:57:19 -0700

    Add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,5 +48,38 @@ module.exports = {
         cookieDomain: 'auto',
       },
     },
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: './src/favicon.png',
+
+        // WebApp Manifest Configuration
+        appName: null, // Inferred with your package.json
+        appDescription: 'Brandon Ferrer Personal WebApp',
+        developerName: 'Brandon Ferrer',
+        developerURL: 'www.webwavy.com',
+        dir: 'auto',
+        lang: 'en-US',
+        background: '#fff',
+        theme_color: '#fff',
+        display: 'standalone',
+        orientation: 'portrait',
+        start_url: '/?homescreen=1',
+        version: '1.0',
+
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          opengraph: false,
+          twitter: false,
+          yandex: false,
+          windows: false,
+        },
+      },
+    },
   ],
 }
