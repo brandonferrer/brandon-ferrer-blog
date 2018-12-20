27924834c1a214d6518edb1567ff022d3c6aefb8
Brandon Ferrer <brandonferrer@peachjar.com>
2018-12-20 09:12:06 -0800

    Update youtube route to vlog

--- src/pages/vlog.jsx
+++ src/pages/vlog.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Helmet from 'react-helmet'
 import Layout from '../components/layout'
 
-const YouTube = () => (
+const Vlog = () => (
   <>
     <Helmet>
       <meta
@@ -19,4 +19,4 @@ const YouTube = () => (
   </>
 )
 
-export default YouTube
+export default Vlog
